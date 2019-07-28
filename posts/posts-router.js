e38d0c369f000dda3cbb9e3e603b9a430b9b87aa
unknown <will.ediger@gmail.com>
2019-07-28 17:23:40 -0500

    updated "get post" error messages to match spec

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -26,14 +26,16 @@ router.get("/:id", async (req, res) => {
     if (post) {
       res.status(200).json(post);
     } else {
-      res.status(404).json({ message: "Post not found" });
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
     }
   } catch (error) {
     // log error to server
     console.log(error);
-    res.status(500).json({
-      message: "Error retrieving the post"
-    });
+    res
+      .status(500)
+      .json({ error: "The post information could not be retrieved." });
   }
 });
 
